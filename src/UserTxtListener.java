ed64ad814704af5ad6131b973c939db0f01d6abc
Howard Chien <h3753880@gmail.com>
2018-03-06 16:28:33 -0800

    user completed

---
+++ src/UserTxtListener.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class UserTxtListener implements DocumentListener {
+	JTextField member;
+	JTextField review;
+	JTextField friend;
+	JTextField star;
+	JComboBox revBox;
+	JComboBox friendBox;
+	JComboBox starBox;
+	JComboBox selectBox;
+	JTextArea queryArea;
+
+	public UserTxtListener(JTextField member, JTextField review, JTextField friend, JTextField star, 
+			JComboBox revBox, JComboBox friendBox, JComboBox starBox, JComboBox selectBox, JTextArea queryArea) {
+		// TODO Auto-generated constructor stub
+		this.member = member;
+		this.review = review;
+		this.friend = friend;
+		this.star = star;
+		this.revBox = revBox;
+		this.friendBox = friendBox;
+		this.starBox = starBox;
+		this.selectBox = selectBox;
+		this.queryArea = queryArea;
+	}
+
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		update();
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		update();
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		update();
+		
+	}
+
+	//select * from users u where u.yelping_since = to_date('2013-05-01', 'yyyy-mm-dd') and 5=(select count(*) from user_friends f where u.user_id=f.user_id)
+	private void update() {
+		String concat = selectBox.getSelectedItem().toString();
+		String begin = "SELECT * FROM USERS U \nWHERE ";
+		StringBuilder sb = new StringBuilder(begin);
+		
+		if(!member.getText().isEmpty()) {
+			sb.append("YELPING_SINCE = TO_DATE('").append(member.getText()).append("-01', 'yyyy-mm-dd') ");
+		}
+		
+		if(!review.getText().isEmpty()) {
+			if(!sb.toString().endsWith("WHERE ")) {
+				sb.append(concat).append(" ");
+			}
+			
+			sb.append("REVIEW_COUNT").append(revBox.getSelectedItem().toString()).append(review.getText()).append(" ");
+		}
+		
+		if(!star.getText().isEmpty()) {
+			if(!sb.toString().endsWith("WHERE ")) {
+				sb.append(concat).append(" ");
+			}
+			
+			sb.append("AVERAGE_STARS").append(starBox.getSelectedItem().toString()).append(star.getText()).append(" ");
+		}
+		
+		if(!friend.getText().isEmpty()) {
+			if(!sb.toString().endsWith("WHERE ")) {
+				sb.append(concat).append(" ");
+			}
+			
+			sb.append("(SELECT COUNT(*) FROM USER_FRIENDS F WHERE U.USER_ID=F.USER_ID)");
+			sb.append(friendBox.getSelectedItem().toString()).append(friend.getText());
+		}
+ 		
+		if(!begin.equals(sb.toString()))
+			queryArea.setText(sb.toString());
+	}
+}
--- src/YFrame.java
+++ src/YFrame.java
@@ -2,6 +2,10 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
@@ -14,20 +18,36 @@ import java.awt.Font;
 import javax.swing.JTable;
 import java.awt.GridLayout;
 import javax.swing.JComboBox;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.*;
+import javax.swing.JCheckBox;
 
 public class YFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private String[] compare = {"=", ">", "<"};
-	private String[] compare2 = {"AND", "OR", "BETWEEN"};
+	private String[] compare2 = {"AND", "OR"};
 	private JPanel userPn;
 	private JTable table;
+	private JTextArea queryArea; //query result
+	private JTextField meTxt1; //member since
+	private JTextField revCountTxt1;
+	private JTextField friendTxt;
+	private JTextField starTxt;
+	private JComboBox revCbox;
+	private JComboBox friendCbox;
+	private JComboBox starCbox;
+	private JComboBox selectCbox;
+	private String preRevBox = "=";
+	private String preFriendBox = "=";
+	private String preStarBox = "=";
 	
 	/**
 	 * Create the frame.
@@ -46,6 +66,18 @@ public class YFrame extends JFrame {
 		queryPanel(); //query part
 		
 		resultPanel(); //result
+		
+		addListener();
+
+		JLabel mainLb = new JLabel("Category");
+		mainLb.setFont(new Font("Serif", Font.PLAIN, 18));
+		mainLb.setBounds(66, 4, 65, 27);
+		contentPane.add(mainLb);
+		
+		JScrollPane maincatScroll = new JScrollPane();
+		maincatScroll.setBounds(10, 37, 210, 346);
+		contentPane.add(maincatScroll);
+		
 		
 	}
 
@@ -71,7 +103,12 @@ public class YFrame extends JFrame {
 				ArrayList<Object> rows = new ArrayList<>();
 				
 				for(String name: col) {
-					rows.add(res.getString(name));
+					String temp = res.getString(name);
+					
+					if(!name.equals("YELPING_SINCE"))
+						rows.add(temp);
+					else
+						rows.add(temp.substring(0, 7));
 				}
 				
 				data.add(rows);
@@ -96,14 +133,73 @@ public class YFrame extends JFrame {
 		
 		db.closeDB();
 	}
+	
+	private void queryReviewByUserId(String userId) {
+		DBconn db = new DBconn();
+		
+		ResultSet res = db.executeQuery("SELECT * FROM REVIEWS WHERE USER_ID='" + userId + "'");
+		ResultSetMetaData meta;
+		int colCount = 0;
+		ArrayList<String> col = new ArrayList<String>();
+		List<List<Object>> data = new ArrayList<>();
+		
+		try {
+			meta = res.getMetaData();
+			colCount = meta.getColumnCount();
+			
+			for(int i=1; i<=colCount; i++) {
+				col.add(meta.getColumnName(i));
+			}
+			
+			while(res.next()) {
+				ArrayList<Object> rows = new ArrayList<>();
+				
+				for(String name: col) {
+					String temp = res.getString(name);
+					
+					rows.add(temp);
+				}
+				
+				data.add(rows);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		DefaultTableModel mod = new DefaultTableModel();
+		
+		for(String c: col)
+			mod.addColumn(c);
+		
+		for(List<Object> ea: data)
+			mod.addRow(ea.toArray());
+		
+		table.setModel(mod);
+		
+		db.closeDB();
+	}
 	  
 	private void resultPanel() {
 		table = new JTable();
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		
+		table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if(table.getSelectedRow() != -1 && !e.getValueIsAdjusting()) {
+					System.out.println(table.getValueAt(table.getSelectedRow(), 0)); //print user_id
+					
+					queryReviewByUserId(table.getValueAt(table.getSelectedRow(), 0).toString());
+				}
+			}
+		});
+		
 		JScrollPane resultScroll = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		resultScroll.setBounds(807, 29, 467, 623);
 		contentPane.add(resultScroll);
+		
 	}
 	
 	private void queryPanel() {
@@ -117,7 +213,7 @@ public class YFrame extends JFrame {
 		queryLb.setFont(new Font("Serif", Font.PLAIN, 18));
 		contentPane.add(queryLb);
 		
-		JTextArea queryArea = new JTextArea();
+		queryArea = new JTextArea();
 		JScrollPane queryScroll = new JScrollPane(queryArea);
 		queryScroll.setBounds(460, 427, 338, 180);
 		contentPane.add(queryScroll);
@@ -149,44 +245,87 @@ public class YFrame extends JFrame {
 		
 		JLabel memLb = new JLabel("Member since:");
 		userPn.add(memLb);
-		JTextField meTxt1 = new JTextField();
+		meTxt1 = new JTextField();
 		userPn.add(meTxt1);
-		JLabel memtLb2 = new JLabel("(YYYY-MM-DD)");
+		JLabel memtLb2 = new JLabel("(YYYY-MM)");
 		userPn.add(memtLb2);
 		JLabel emptyLb1 = new JLabel("");
 		userPn.add(emptyLb1);
 		
+		
 		JLabel memLb2 = new JLabel("Review Count:");
 		userPn.add(memLb2);
-		JComboBox revCbox = new JComboBox(compare);
+		revCbox = new JComboBox(compare);
 		userPn.add(revCbox);
 		JLabel valLb = new JLabel("Value:");
 		userPn.add(valLb);
-		JTextField revCountTxt1 = new JTextField();
+		revCountTxt1 = new JTextField();
 		userPn.add(revCountTxt1);
+		revCbox.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				queryArea.setText(queryArea.getText().replace("REVIEW_COUNT" + preRevBox, "REVIEW_COUNT" + revCbox.getSelectedItem().toString()));
+				preRevBox = revCbox.getSelectedItem().toString();
+			}
+		});
 		
 		JLabel memLb3 = new JLabel("Number of Friends:");
 		userPn.add(memLb3);
-		JComboBox revCbox2 = new JComboBox(compare);
-		userPn.add(revCbox2);
+		friendCbox = new JComboBox(compare);
+		userPn.add(friendCbox);
 		JLabel valLb2 = new JLabel("Value:");
 		userPn.add(valLb2);
-		JTextField friendTxt = new JTextField();
+		friendTxt = new JTextField();
 		userPn.add(friendTxt);
+		friendCbox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String s = "(SELECT COUNT(*) FROM USER_FRIENDS F WHERE U.USER_ID=F.USER_ID)";
+				queryArea.setText(queryArea.getText().replace(s + preFriendBox, s + friendCbox.getSelectedItem().toString()));
+				preFriendBox = friendCbox.getSelectedItem().toString();
+			}
+		});
+		
 		
 		JLabel memLb4 = new JLabel("Avg. Stars:");
 		userPn.add(memLb4);
-		JComboBox revCbox3 = new JComboBox(compare);
-		userPn.add(revCbox3);
+		starCbox = new JComboBox(compare);
+		userPn.add(starCbox);
 		JLabel valLb3 = new JLabel("Value:");
 		userPn.add(valLb3);
-		JTextField starTxt = new JTextField();
+		starTxt = new JTextField();
 		userPn.add(starTxt);
+		starCbox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				queryArea.setText(queryArea.getText().replace("AVERAGE_STARS" + preStarBox, "AVERAGE_STARS" + starCbox.getSelectedItem().toString()));
+				preStarBox = starCbox.getSelectedItem().toString();
+			}
+		});
 		
 		
 		JLabel memLb5 = new JLabel("Select:");
 		userPn.add(memLb5);
-		JComboBox revCbox4 = new JComboBox(compare2);
-		userPn.add(revCbox4);
+		selectCbox = new JComboBox(compare2);
+		userPn.add(selectCbox);
+		selectCbox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String concat = selectCbox.getSelectedItem().toString();
+				queryArea.setText(queryArea.getText().replace("AND", concat));
+				queryArea.setText(queryArea.getText().replace("OR", concat));
+			}
+		});
+	}
+	
+	private void addListener() {
+		DocumentListener listener = new UserTxtListener(meTxt1, revCountTxt1, friendTxt, starTxt, 
+				revCbox, friendCbox, starCbox, selectCbox, queryArea);
+
+		meTxt1.getDocument().addDocumentListener(listener);
+		revCountTxt1.getDocument().addDocumentListener(listener);
+		friendTxt.getDocument().addDocumentListener(listener);
+		starTxt.getDocument().addDocumentListener(listener);
 	}
 }
